6bcd610f0d15294bff259b286911e263e1e952ec
黄溪舟 <xizhouh@gmail.com>
2017-11-30 20:21:38 -0600

    :bug: Fix bugs

--- src/app.js
+++ src/app.js
@@ -11,31 +11,27 @@ if (process.browser) {
   require('videojs-contrib-hls/dist/videojs-contrib-hls')
   Vue.use(VueVideoPlayer)
 
-
-  function formatComponentName(vm)
-  {
-    if (vm.$root === vm) return 'root';
-
-    var name = vm._isVue ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag) : vm.name;
-    return (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ' at ' + (vm.$options && vm.$options.__file) : '');
-
+  // 引入 Fundebug 监控
+  const formatComponentName = (vm) => {
+    if (vm.$root === vm) return 'root'
+    let name = vm._isVue
+      ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag)
+      : vm.name
+    return (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ' at ' + (vm.$options && vm.$options.__file) : '')
   }
 
-  Vue.config.errorHandler = function(err, vm, info)
-  {
-    var componentName = formatComponentName(vm);
-    var propsData = vm.$options && vm.$options.propsData;
-
-    fundebug.notifyError(err,
-    {
-        metaData:
-        {
-            componentName: componentName,
-            propsData: propsData,
-            info: info
-        }
-     });
-  };
+  Vue.config.errorHandler = (err, vm, info) => {
+    let componentName = formatComponentName(vm)
+    let propsData = vm.$options && vm.$options.propsData
+    fundebug.notifyError(err, {
+      metaData:
+      {
+        componentName: componentName,
+        propsData: propsData,
+        info: info
+      }
+    })
+  }
 }
 
 Vue.mixin(titleMixin)
